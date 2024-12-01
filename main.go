01098ca8afd4ad5b2ad7a4d0eead31bafdc56b8d
Marc Obel <mao@syseng.dk>
2024-12-01 22:08:19 +0100

    setup missing.css and htmx and html template handler

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
+
+type PageData struct {
+	Title   string
+	Message string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/template.html")
+	if err != nil {
+		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		return
+	}
+
+	data := PageData{
+		Title:   "Welcome to Hyper Rogue!",
+		Message: "This HTML is rendered dynamically from a template.",
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, Go project!")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
