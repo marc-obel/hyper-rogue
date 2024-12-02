7d8c45c3b624fa654140256138e49ddf8a260e7a
Marc Obel <mao@syseng.dk>
2024-12-02 21:30:15 +0100

    render grid works

--- main.go
+++ main.go
@@ -5,21 +5,41 @@ import (
 	"net/http"
 )
 
+type Cell struct {
+	X           int
+	Y           int
+	Class       string
+	DisplayChar string
+}
+
 type PageData struct {
-	Title   string
-	Message string
+	Grid []Cell
+}
+
+const (
+	GridWidth  = 15
+	GridHeight = 15
+)
+
+var grid []Cell
+
+func main() {
+	initializeGrid()
+	http.HandleFunc("/", handler)
+
+	println("Starting server on :8080")
+	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/template.html")
+	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
 		http.Error(w, "Error loading template", http.StatusInternalServerError)
 		return
 	}
 
 	data := PageData{
-		Title:   "Welcome to Hyper Rogue!",
-		Message: "This HTML is rendered dynamically from a template.",
+		Grid: grid,
 	}
 
 	err = tmpl.Execute(w, data)
@@ -28,7 +48,17 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+func initializeGrid() {
+	grid = make([]Cell, GridWidth*GridHeight)
+	for y := 0; y < GridHeight; y++ {
+		for x := 0; x < GridWidth; x++ {
+			index := y*GridWidth + x
+			grid[index] = Cell{
+				X:           x,
+				Y:           y,
+				Class:       "",
+				DisplayChar: "•",
+			}
+		}
+	}
 }
